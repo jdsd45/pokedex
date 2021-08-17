2cb23b8eb4074a7bbe26c47ab20dbbd27633bcd8
jdsd45 <jdsd45@gmail.com>
2021-08-17 18:29:31 +0200

    maj app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
