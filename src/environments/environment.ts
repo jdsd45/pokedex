6103464fe4e7fcbc672ed6997081d09a91977e43
jdsd45 <jdsd45@gmail.com>
2021-08-19 08:17:12 +0200

    env api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://pokeapi.co/api/v2',
+  apiRessources: {
+    pokemon: {
+      pokemonSpecies: '/pokemon-species'
+    }
+  }
 };
 
 /*
